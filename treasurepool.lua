d3cf51cf1de51228fc5fe514cd988bd21b7f6938
Zaldas <anas_alkhatib@hotmail.com>
2026-05-23 23:07:08 -0500

    chore: version bump to 2.5

--- treasurepool.lua
+++ treasurepool.lua
@@ -1,6 +1,6 @@
 addon.name      = 'TreasurePool'
 addon.author    = 'Shiyo, Zaldas'
-addon.version   = '2.4'
+addon.version   = '2.5'
 addon.desc      = 'Displays your current treasure pool with lot/pass buttons.'
 addon.link      = 'https://ashitaxi.com/'
 
